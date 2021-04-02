fb7bef2e8a77daa4777d836f6cac5ca615bf00c4
chandraxf <cacan.dira@gmail.com>
2021-04-03 04:07:09 +0800

    Perhitungan nilai berdasarkan grade

---
+++ perhitungan.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//dibuat oleh chandra endira
+public class perhitungan {
+    public static void main(String[] args) {
+
+        int nilai;
+        String grade;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inputkan nilai: ");
+        nilai = scanner.nextInt();
+
+        if ( nilai >= 90 ) {
+            grade = "A";
+        } else if ( nilai >= 80 ){
+            grade = "B+";
+        } else if ( nilai >= 70 ){
+            grade = "B";
+        } else if ( nilai >= 60 ){
+            grade = "C+";
+        } else if ( nilai >= 50 ){
+            grade = "C";
+        } else if ( nilai >= 40 ){
+            grade = "D";
+        } else {
+            grade = "E";
+        }
+
+        System.out.println("Grade: " + grade);
+    }
+}
