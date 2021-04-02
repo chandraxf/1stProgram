876ed2a914180cbd887a9e67b40eb34cf10bf0cc
chandraxf <cacan.dira@gmail.com>
2021-04-03 03:23:19 +0800

    Penambahan data alamat

--- chandra.java
+++ chandra.java
@@ -1,7 +1,9 @@
 import datadiri.data;
+import datadiri.alamat;
 public class chandra{
 	public static void main(String[]args){
 		System.out.println("Ini adalah program pertama saya");
 		data.datax();
+		alamat.info();
 	}
 }
---
+++ datadiri/alamat.java
@@ -0,0 +1,14 @@
+package datadiri;
+
+public class alamat{
+	public static void info(){
+		System.out.println();
+		System.out.println("======================================");
+		System.out.println("= Alamat   : Jl. Dr. Adam Malik      =");
+		System.out.println("=            Kuala Pembuang/Kalteng  =");
+		System.out.println("= No. HP   : 081351718752 = Wa/Tele  =");
+		System.out.println("= Agama    : Islam                   =");
+		System.out.println("= Kode Pos : 74212                   =");
+		System.out.println("= ==================================== ");
+		}
+	}
